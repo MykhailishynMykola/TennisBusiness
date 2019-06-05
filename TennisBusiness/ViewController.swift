e3f0476751749f30781cbdf14f1ba01174b92a3b
Nikolay Mikhaylishin <nikolay.mikhaylishin@3ss.tv>
2019-06-05 15:00:51 +0300

    some refactoring

--- TennisBusiness/ViewController.swift
+++ TennisBusiness/ViewController.swift
@@ -18,57 +18,64 @@ class ViewController: UIViewController {
             guard worlds.count == expectedWorldsCount else {
                 return
             }
-            loadMatches()
+            parseMatches()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         database = Firestore.firestore()
-        
-        database.collection("worlds").getDocuments { [weak self] (worldsSnapshot, error) in
-            guard let worldDocuments = worldsSnapshot?.documents,
-                error == nil else {
-                return
-            }
-            self?.expectedWorldsCount = worldDocuments.count
-            for worldDocument in worldDocuments {
-                guard worldDocument.exists else { return }
-                let worldIdentifier = worldDocument.documentID
-                let worldData = worldDocument.data()
-                guard let speed = worldData["speed"] as? Double,
-                    let createdAt = worldData["createdAt"] as? Timestamp else {
-                        return
-                }
-                let playersReference = self?.database.collection("worlds").document(worldDocument.documentID).collection("players")
-                playersReference?.getDocuments { [weak self] (playerSnapshot, error) in
-                    guard let playerDocuments = playerSnapshot?.documents,
-                        error == nil else {
-                        return
-                    }
-                    var players: [String: Player] = [:]
-                    for playerDocument in playerDocuments {
-                        guard playerDocument.exists,
-                            let player = Player(snapshot: playerDocument) else {
-                                return
-                        }
-                        players[playerDocument.documentID] = player
-                    }
-                    let newWorld = World(identifier: worldIdentifier,
-                                         speed: speed,
-                                         createdAt: createdAt.dateValue(),
-                                         players: players)
-                    self?.worlds[worldIdentifier] = newWorld
-                }
-            }
-        }
+        loadData()
     }
     
     
     
     // MARK: - Private
     
-    private func loadMatches() {
+    private func loadData() {
+        database.collection("worlds").getDocuments { [weak self] (worldsSnapshot, error) in
+            guard let worldDocuments = worldsSnapshot?.documents,
+                error == nil else {
+                    return
+            }
+            self?.expectedWorldsCount = worldDocuments.count
+            for worldDocument in worldDocuments {
+                self?.parseWorld(worldDocument)
+            }
+        }
+    }
+    
+    private func parseWorld(_ document: QueryDocumentSnapshot) {
+        guard document.exists else { return }
+        let worldIdentifier = document.documentID
+        let worldData = document.data()
+        guard let speed = worldData["speed"] as? Double,
+            let createdAt = worldData["createdAt"] as? Timestamp else {
+                return
+        }
+        let playersReference = database.collection("worlds").document(document.documentID).collection("players")
+        playersReference.getDocuments { [weak self] (playerSnapshot, error) in
+            guard let playerDocuments = playerSnapshot?.documents,
+                error == nil else {
+                    return
+            }
+            var players: [String: Player] = [:]
+            for playerDocument in playerDocuments {
+                guard playerDocument.exists,
+                    let player = Player(snapshot: playerDocument) else {
+                        return
+                }
+                players[playerDocument.documentID] = player
+            }
+            let newWorld = World(identifier: worldIdentifier,
+                                 speed: speed,
+                                 createdAt: createdAt.dateValue(),
+                                 players: players)
+            self?.worlds[worldIdentifier] = newWorld
+        }
+    }
+    
+    private func parseMatches() {
         for (identifier, world) in worlds {
             database.collection("worlds").document(identifier).collection("matches").getDocuments { (matchesSnapshot, error) in
                 guard let matchDocuments = matchesSnapshot?.documents else {
